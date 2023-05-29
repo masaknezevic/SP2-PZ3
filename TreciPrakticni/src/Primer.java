efd56f1a533da6ecdb0d8fb2a867cb9d527ca9ce
masaknezevic <134741246+masaknezevic@users.noreply.github.com>
2023-05-29 15:35:43 +0200

    Update Primer.java

--- TreciPrakticni/src/Primer.java
+++ TreciPrakticni/src/Primer.java
@@ -1,12 +1,16 @@
 
 public class Primer {
-	
-	
-	
+
+	/*calcultes sum of first n elements
+	 * 
+	 * n needs to be a positive number
+	 * 
+	 * */
+
 	public int sum(int n) {
 		int result = 0;
-		for(int i = 1; i <= 10; i++) {
-			result += i;
+		for(int i = 1; i < n; i++) {
+			result = i;
 		}
 		return result;
 	}
